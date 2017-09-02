75dd74997e3db8bd02f7616401622a64c9a7ab03
pshah123 <priansh123@me.com>
2017-09-02 01:54:41 -0400

    test script

---
+++ tests/example.py
@@ -0,0 +1,6 @@
+from console_logging import console
+
+console.log("This is a log.")
+console.error("This is an error.")
+console.info("This is some neutral info.")
+console.success("This is a success message.")
