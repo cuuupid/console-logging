4f61cc787fa513bb5066fc905ac5dd80443b26f8
Priansh Shah <shah@priansh.com>
2017-10-12 19:23:13 +0100

    pypi is a little bitch so hard coded in the long_description

--- README.rst
+++ README.rst
@@ -1,40 +1,51 @@
 console-logging
 ===============
 
+|Codeship Status for pshah123/console-logging| |PyPI version| |Codacy
+Badge|
 
 Better console logging for Python.
+
+Find us on PyPi: https://pypi.python.org/pypi/console-logging
 
 .. figure:: https://github.com/pshah123/console-logging/raw/master/images/example.png
    :alt: Demo of console-logging
 
-
+   Showcase
 
 Getting Started
 ---------------
 
-
 Dependencies
 ~~~~~~~~~~~~
-
 
 -  Python 2.6+ or Python 3.5+
 -  ``termcolor``
 
-
-
 Installation
 ~~~~~~~~~~~~
-
 
 ::
 
     pip install console-logging
 
-
+*If building from source:* ``bash pipe`` from inside this repo.
 
 Usage
 ~~~~~
 
+New:
+^^^^
+
+::
+
+    from console_logging.console import Console
+    console = Console()
+
+    console.log("Hello world!")
+
+Old:
+^^^^
 
 ::
 
@@ -42,11 +53,8 @@ Usage
 
     console.log("Hello World!")
 
-
-
 Exhaustive Reference
 ~~~~~~~~~~~~~~~~~~~~
-
 
 ::
 
@@ -55,18 +63,30 @@ Exhaustive Reference
     console.info("This is some neutral info.")
     console.success("This is a success message.")
 
-
+    # If using the new usage:
+    console.setVerbosity(4) # verbosity from 1 - 5, in order:
+    '''
+    [1] Errors
+    [2] Successes
+    [3] Logs
+    [4] Info
+    [5] Secure
+    '''
+    console.mute() # shorthand for setVerbosity(0)
 
 Example
 ~~~~~~~
 
-
 For an exhaustive example, see ``tests/example.py``.
-
-
 
 Credit
 ~~~~~~
 
-
 -  ``termcolor`` module for colors
+
+.. |Codeship Status for pshah123/console-logging| image:: https://app.codeship.com/projects/aed26890-8fca-0135-1d5e-36d54fbb9242/status?branch=master
+   :target: https://app.codeship.com/projects/250054
+.. |PyPI version| image:: https://badge.fury.io/py/console-logging.svg
+   :target: https://badge.fury.io/py/console-logging
+.. |Codacy Badge| image:: https://api.codacy.com/project/badge/Grade/c6656875f3234dfb9ab6bd4aa509ff57
+   :target: https://www.codacy.com/app/pshah123/console-logging?utm_source=github.com&utm_medium=referral&utm_content=pshah123/console-logging&utm_campaign=Badge_Grade
--- setup.py
+++ setup.py
@@ -1,15 +1,112 @@
 from setuptools import setup, find_packages
-from codecs import open as copen
+from codecs import open as open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with copen(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+print(long_description)
+
+long_description = """
+console-logging
+===============
+
+|Codeship Status for pshah123/console-logging| |PyPI version| |Codacy
+Badge|
+
+Better console logging for Python.
+
+Find us on PyPi: https://pypi.python.org/pypi/console-logging
+
+.. figure:: https://github.com/pshah123/console-logging/raw/master/images/example.png
+   :alt: Demo of console-logging
+
+   Showcase
+
+Getting Started
+---------------
+
+Dependencies
+~~~~~~~~~~~~
+
+-  Python 2.6+ or Python 3.5+
+-  ``termcolor``
+
+Installation
+~~~~~~~~~~~~
+
+::
+
+    pip install console-logging
+
+*If building from source:* ``bash pipe`` from inside this repo.
+
+Usage
+~~~~~
+
+New:
+^^^^
+
+::
+
+    from console_logging.console import Console
+    console = Console()
+
+    console.log("Hello world!")
+
+Old:
+^^^^
+
+::
+
+    from console_logging import console
+
+    console.log("Hello World!")
+
+Exhaustive Reference
+~~~~~~~~~~~~~~~~~~~~
+
+::
+
+    console.log("This is a log.")
+    console.error("This is an error.")
+    console.info("This is some neutral info.")
+    console.success("This is a success message.")
+
+    # If using the new usage:
+    console.setVerbosity(4) # verbosity from 1 - 5, in order:
+    '''
+    [1] Errors
+    [2] Successes
+    [3] Logs
+    [4] Info
+    [5] Secure
+    '''
+    console.mute() # shorthand for setVerbosity(0)
+
+Example
+~~~~~~~
+
+For an exhaustive example, see ``tests/example.py``.
+
+Credit
+~~~~~~
+
+-  ``termcolor`` module for colors
+
+.. |Codeship Status for pshah123/console-logging| image:: https://app.codeship.com/projects/aed26890-8fca-0135-1d5e-36d54fbb9242/status?branch=master
+   :target: https://app.codeship.com/projects/250054
+.. |PyPI version| image:: https://badge.fury.io/py/console-logging.svg
+   :target: https://badge.fury.io/py/console-logging
+.. |Codacy Badge| image:: https://api.codacy.com/project/badge/Grade/c6656875f3234dfb9ab6bd4aa509ff57
+   :target: https://www.codacy.com/app/pshah123/console-logging?utm_source=github.com&utm_medium=referral&utm_content=pshah123/console-logging&utm_campaign=Badge_Grade
+"""
 
 setup(
     name='console-logging',
-    version='0.0.3.1',
+    version='0.0.3.8',
     description='Better, prettier commandline logging.',
     long_description=long_description,
     url='https://github.com/pshah123/console-logging',
